c374975248731409f58778a59e98c32780e12387
Abkels <abrahamk29@gmail.com>
2023-10-20 22:05:16 +0100

    mock

--- script.js
+++ script.js
@@ -8,11 +8,24 @@ const googleDatabase = [
     'myfavoritecat2.com',
 ]
 
-const googleSearch = (searchinput) => {
-    const matches = googleDatabase.filter ((website) => {
-        return website.includes(searchinput)
+// const googleSearch = (searchinput) => {
+//     const matches = googleDatabase.filter ((website) => {
+//         return website.includes(searchinput);
+//     })
+//     return matches.length > 3 ? matches.slice(0,3) : matches;
+// }
+
+// applying dependency injection by adding another parameter to the function
+
+const googleSearch = (searchinput, db) => {
+    const matches = db.filter ((website) => {
+        return website.includes(searchinput);
     })
     return matches.length > 3 ? matches.slice(0,3) : matches;
 }
 
-console.log (googleSearch('flow'))
+// console.log (googleSearch('flower', googleDatabase));
+
+module.exports = googleSearch
+
+/*In real life situation you will not be exporting a database into a test and have the test go into a database. It will be too expensive, slow and a headache. You will have to mock it, for which the mock will have a dummy database for we to test on so iy=t can be faster */
--- script.test.js
+++ script.test.js
@@ -1,3 +1,19 @@
+const googleSearch = require('./script') 
+ 
+dbMock = [
+    'dog.com',
+    'cheesepuff.com',
+    'disney.com',
+    'dogpictures.com'
+];
+
  it('this is a test', ()=> {
-    
+    // console.log(googleSearch) 
+    expect('hello').toBe('hello')
+    googleSearch('testtest', dbMock)
+ })
+
+ it('it is searching google', ()=> {
+    expect(googleSearch('testtest', dbMock)).toEqual([])
+    expect(googleSearch('dog', dbMock)).toEqual(['dog.com','dogpictures.com'])
  })
