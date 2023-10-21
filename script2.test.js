85361b56f81ea277433b313698ada914cd37ddad
Abkels <abrahamk29@gmail.com>
2023-10-21 11:17:19 +0100

    mock and spies in asynchronous test

--- script2.test.js
+++ script2.test.js
@@ -26,9 +26,28 @@ it('call swapi to get people', ()=>{
     });
 })
 
+// Mocks and Spies
+// creating a mock
+it('get people returns count and result', ()=> {
+    const mockFetch = jest.fn() //allows you to spy in jest
+    .mockreturnValue(Promise.resolve({
+        json: ()=> Promise.resolve({
+            count: 87,
+            results: [0,1,2,3,4,5]
+        })
+    }))
 
+    expect.assertions(4)
+    return swapi.getPeople(mockFetch).then(data => {
+        expect(mockFetch.mockcalls.length).toBe(1);
+        expect(mockFetch).toBeCalledWith('https://swapi.dev/api/people');
+        expect(data.count).toEqual(87)
+        expect(data.results.length).toBeGreaterThan(5)
+    })
+})
 
 /*When running an asynchronous test always do expect.assertions to test that ur assertions are being tested 
 
 u can check for jest cheatsheet to help you with testing
 */
+
